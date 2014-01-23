532fac75c57d9c6c4014c4aa507bfca76dbc35c7
dranelol <cardsfan853@gmail.com>
2014-01-23 11:32:07 -0600

    initial commit

---
+++ StateMachineTest/State.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceIsFun
+{
+    class State
+    {
+        /// <summary>
+        /// name of the state
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// property for name
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        /// <summary>
+        /// the method delegate for entering the state
+        /// </summary>
+
+        public delegate void Enter();
+
+        /// <summary>
+        /// the method delegate for leaving the state
+        /// </summary>
+        public delegate void Leave();
+
+        /// <summary>
+        /// the method delegate for updating the state
+        /// </summary>
+        /// <param name="gameTime">current game time</param>
+        public delegate void Update();
+
+        /// <summary>
+        /// event called on state enter
+        /// </summary>
+        public event Enter enter;
+
+        /// <summary>
+        /// event called on state leave
+        /// </summary>
+        public event Leave leave;
+
+        /// <summary>
+        /// event called on state update
+        /// </summary>
+        public event Update update;
+
+
+        /// <summary>
+        /// the function executed on state enter
+        /// </summary>
+        internal void execEnter()
+        {
+            System.Diagnostics.Debug.WriteLine("entering: " + name);
+            enter();
+        }
+
+        /// <summary>
+        /// the function executed on state leave
+        /// </summary>
+        internal void execLeave()
+        {
+            System.Diagnostics.Debug.WriteLine("leaving: " + name);
+            leave();
+        }
+
+        /// <summary>
+        /// the function executed on state update
+        /// </summary>
+        internal void execUpdate()
+        {
+            update();
+        }
+
+        /// <summary>
+        /// a mapping, from string to state, of state transitions
+        /// </summary>
+        Dictionary<string, State> transitions = new Dictionary<string, State>();
+
+        /// <summary>
+        /// property for transitions
+        /// </summary>
+        public Dictionary<string, State> Transitions
+        {
+            get
+            {
+                return transitions;
+
+            }
+        }
+    }
+}
---
+++ StateMachineTest/StateMachine.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceIsFun
+{
+    class StateMachine
+    {
+        #region fields
+
+
+        /// <summary>
+        /// the current state in execution
+        /// </summary>
+        private State currentState = null;
+        
+        /// <summary>
+        /// property for currentState
+        /// </summary>
+        public State CurrentState
+        {
+            get
+            {
+                return currentState;
+            }
+        }
+
+        /// <summary>
+        /// the previous state in execution
+        /// </summary>
+        private State previousState = null;
+
+        /// <summary>
+        /// property for previousState
+        /// </summary>
+        public State PreviousState
+        {
+            get
+            {
+                return previousState;
+            }
+        }
+
+        /// <summary>
+        /// is the state machine started?
+        /// </summary>
+        private bool started = false;
+
+        public bool Started
+        {
+            get
+            {
+                return started;
+            }
+
+            set
+            {
+                started = value;
+            }
+        }
+
+        #endregion
+
+        #region constructors / destructors
+        public StateMachine()
+        {
+        }
+
+        #endregion
+
+        
+        #region methods
+
+        /// <summary>
+        /// start the state machine
+        /// </summary>
+        /// <param name="startState">the state to start with</param>
+        public void Start(State startState)
+        {
+            if (currentState == null)
+            {
+                currentState = startState;
+            }
+        }
+
+        /// <summary>
+        /// init the state machine, possible state setup
+        /// </summary>
+        /// <param name="startState">the state to start with</param>
+        public void Initialize(State startState)
+        {
+        }
+
+        /// <summary>
+        /// update the state machine
+        /// </summary>
+        public void Update()
+        {
+            // if there's no current state running, and we've somehow tried to update the state machine, throw an exception
+            if (currentState == null)
+            {
+                throw new NullReferenceException("no start state");
+            }
+
+            // if we havent started yet, start!
+            if (started == false)
+            {
+                currentState.execEnter();
+                started = true;
+            }
+
+            //System.Diagnostics.Debug.WriteLine("current state: " + currentState.Name);
+
+            // update the current state
+            currentState.execUpdate();
+        }
+
+        /// <summary>
+        /// transition to the next state in execution
+        /// </summary>
+        /// <param name="nextState">the name of the state to transition to</param>
+        public void Transition(string nextState)
+        {
+            System.Diagnostics.Debug.WriteLine(nextState);
+            // if there's no current state running, and we've somehow tried to  transition, throw an exception
+            if(currentState == null)
+            {
+                throw new NullReferenceException("no current state");
+            }
+
+            // get the next state from the current state's list of transitions
+            State next = currentState.Transitions[nextState];
+
+            // if no state was returned, then the current state cannot transition to that state
+            if(nextState == null)
+            {
+                throw new NullReferenceException("no state to transition to");
+            }
+
+            // leave the current state
+            currentState.execLeave();
+            // set the state objects correctly
+            previousState = currentState;
+            currentState = next;
+            // enter the (new) current state
+            currentState.execEnter();
+        }
+
+        #endregion
+
+    }
+}
+
+
